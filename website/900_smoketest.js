fd1766fdacc06bffa4a9363538176118776f34e3
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2024-08-17 17:41:23 +0200

    added all demos from the spring-bds demo app

---
+++ website/900_smoketest.js
@@ -0,0 +1,6 @@
+import { balIconConsultant } from 'https://cdn.jsdelivr.net/npm/@baloise/ds-icons/index.esm.js';
+import { balBrandIconTheftCarGreen } from 'https://cdn.jsdelivr.net/npm/@baloise/ds-brand-icons/index.esm.js';
+
+document.getElementById('balIconConsultant').svg = balIconConsultant;
+document.getElementById('balBrandIconTheftCarGreen').svg = balBrandIconTheftCarGreen;
+
