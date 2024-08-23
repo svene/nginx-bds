90f3881a4808c3be97c4157ccfb4c09d9afd5020
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2024-08-23 19:03:01 +0200

    improved button examples

--- website/002_button.js
+++ website/002_button.js
@@ -1,18 +1,33 @@
-const listenerButton = document.getElementById('listenerButton');
-const linkBtn = document.getElementById('linkBtn');
-const output = document.getElementById('output');
-
-listenerButton.addEventListener('click', (event) => {
-	console.log('click listener', event);
-	output.innerText = 'output: click listener';
-});
-linkBtn.addEventListener('balNavigate', (event) => console.log('linkBtn: balNavigate', event));
-linkBtn.addEventListener('click', (event) => console.log('linkBtn: click', event));
-
-export function ccc(event) {
-	output.innerText = 'output: onclick attribute';
+// Card 1:
+const output1 = document.getElementById('output1');
+export function handleClickFromAttribute(element, event) {
+	console.log(element	);
+	console.log(event);
+	console.log(event.target);
+	output1.innerText = 'output: onclick attribute';
 }
 
+// Card 2:
+const listenerButton = document.getElementById('listenerButton');
+const output2 = document.getElementById('output2');
+listenerButton.addEventListener('click', (event) => { // note: (element, event) does not work. Only one parameter (the event) is passed id
+	console.log('click listener: event: ', event);
+	console.log('click listener: event.target: ', event.target);
+	output2.innerText = 'output: click event from listenerButton';
+});
+
+const linkBtn = document.getElementById('linkBtn');
+
+linkBtn.addEventListener('balNavigate', (event) => { // TODO-DS: event 'balNavigate' not emitted
+	console.log('linkBtn: balNavigate', event);
+});
+linkBtn.addEventListener('click', (event) => {
+	output2.innerText = 'output: click event from linkBtn';
+	console.log('linkBtn: click', event);
+});
+
+
+// global:
 window.app = {
-	ccc: ccc,
+	handleClickFromAttribute: handleClickFromAttribute,
 }
